a0498a0945b59bcf74a83b5c355d9cc593f23af1
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-10-11 10:52:28 -0500

    finished all challenges

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -21,24 +21,45 @@ namespace ChallengesWithTestsMark8
 
         public bool IsPrimeNumber(int num)
         {
-            if  (num <= 0) return false;
-            return Enumerable.Range(1, num).Any(num => num -1 % num == 0);
+            if  (num <= 1) return false;
+            return Enumerable.Range(2, num - 2).All(n => num % n != 0);
             
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            var uniques = str.Where(x => str.Count(y => y == x) <= 1);
+            if (uniques.Count() == 0) return -1;
+            return str.IndexOf(uniques.Last());
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Input array is empty or null.");
+            }
+            int maxCount = numbers.Length > 0 ? 1 : 0;
+            int currentCount = 1;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentCount++;
+                    maxCount = Math.Max(maxCount, currentCount);
+                }
+                else
+                {
+                    currentCount = 1;
+                }
+            }
+            return maxCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count < n) return new double[] { };
+            return elements.Where(element => elements.IndexOf(element) % n == n - 1).ToArray();
         }
     }
 }
